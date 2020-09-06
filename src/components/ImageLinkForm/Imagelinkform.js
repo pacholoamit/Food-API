6b1e5c79ea2cc0464655d8779726de027aca018f
pacholoamit <pacholoamit@github.com>
2020-09-06 14:21:10 +0800

    update

--- src/components/ImageLinkForm/Imagelinkform.js
+++ src/components/ImageLinkForm/Imagelinkform.js
@@ -9,8 +9,8 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 				{'Input any food image and I will detect what food it is.'}
 			</p>
 			<div className='center'>
-				<div className='form center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-70 center' type= 'tex' onChange={onInputChange}/>
+				<div className='form center pa3 br2 shadow-5'>
+					<input placeholder= "Input image link here" className='f4 pa2 w-70 center'  type= 'tex' onChange={onInputChange}/>
 					<button
 						className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
 						onClick={onButtonSubmit}>Detect</button>
